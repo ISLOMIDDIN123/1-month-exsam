1d76846f09492fe79385784e5630f18511cb9db1
ISLOMIDDIN123 <jorayevislomiddin220903@gmail.com>
2023-01-11 17:02:35 +0500

    islomiddin

--- js/main.js
+++ js/main.js
@@ -22,3 +22,19 @@ window.addEventListener("scroll", function () {
     
     document.getElementById("navbar-open").addEventListener("click", openNavbar);
     document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+
+
+    window.addEventListener("scroll", function () {
+      shrink();
+    });
+    
+    let navbar = document.getElementById("navbar");
+    
+    function shrink() {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.classList.add("navbar-shrink");
+      } else {
+        navbar.classList.remove("navbar-shrink");
+      }
+    }
